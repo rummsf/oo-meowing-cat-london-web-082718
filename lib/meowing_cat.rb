a4981f1aac0a5c7667b72908a6e3e43719e05683
Irum F <irummf@gmail.com>
2018-08-31 15:32:40 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,12 +1,12 @@
 class Cat 
-attr_accessor :name 
+  attr_accessor :name 
 
-def meow= (meow)
-  @meow = meow
-end 
+  def meow= (meow)
+    @meow = meow
+  end 
 
-def meow
-  @meow
-puts "meow!"
-end
+  def meow
+    @meow
+    puts "meow!"
+  end
 end 
